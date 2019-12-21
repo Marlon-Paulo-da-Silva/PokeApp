9299a0b2da45055d635efff484804981e44f2223
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-21 00:49:10 -0300

    finalizando tela de detalhes

--- lib/pokemondetail.dart
+++ lib/pokemondetail.dart
@@ -6,30 +6,101 @@ class Pokedetail extends StatelessWidget {
 
   Pokedetail({this.pokemon});
 
-  bodyWidget() => Stack(
+  bodyWidget(BuildContext context) => Stack(
         children: <Widget>[
-          Container(
+          Positioned(
+            height: MediaQuery.of(context).size.height / 1.4,
+            width: MediaQuery.of(context).size.width - 20,
+            left: 10.0,
+            top: MediaQuery.of(context).size.height * 0.15,
             child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text(pokemon.name),
+                  SizedBox(
+                    height: 70.0,
+                  ),
+                  Text(
+                    pokemon.name,
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
                   Text("Height: ${pokemon.height}"),
                   Text("Weight: ${pokemon.weight}"),
-                  Text("Types"),
+                  Text(
+                    "Types",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: pokemon.type
                         .map((t) => FilterChip(
+                              backgroundColor: Colors.amber,
                               label: Text(t),
                               onSelected: (b) {},
                             ))
                         .toList(),
                   ),
-                  Text("Weakness"),
+                  Text(
+                    "Weakness",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: pokemon.weaknesses
+                        .map((t) => FilterChip(
+                              backgroundColor: Colors.red,
+                              label: Text(
+                                t,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              onSelected: (b) {},
+                            ))
+                        .toList(),
+                  ),
+                  Text(
+                    "Next Evolution",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: pokemon.nextEvolution
+                        .map((n) => FilterChip(
+                              backgroundColor: Colors.green,
+                              label: Text(
+                                n.name,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              onSelected: (b) {},
+                            ))
+                        .toList(),
+                  ),
                 ],
               ),
             ),
           ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Hero(
+                tag: pokemon.img,
+                child: Container(
+                  height: 200.0,
+                  width: 200.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(pokemon.img),
+                    ),
+                  ),
+                )),
+          )
         ],
       );
 
@@ -42,7 +113,7 @@ class Pokedetail extends StatelessWidget {
         backgroundColor: Colors.cyan,
         title: Text(pokemon.name),
       ),
-      body: bodyWidget(),
+      body: bodyWidget(context),
     );
   }
 }
